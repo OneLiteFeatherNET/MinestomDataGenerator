45e0b67d7678c31b2873f1635f942b9db32f4702
TheMeinerLP <p.glanz@madfix.me>
2024-02-11 21:05:27 +0100

    Add feature flag generation

--- DataGenerator/src/main/java/net/minestom/generators/FeatureFlagGenerator.java
+++ DataGenerator/src/main/java/net/minestom/generators/FeatureFlagGenerator.java
@@ -4,12 +4,8 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import net.minecraft.world.flag.FeatureFlags;
 import net.minestom.datagen.DataGenerator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public final class FeatureFlagGenerator extends DataGenerator {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(FeatureFlagGenerator.class);
 
     @Override
     public JsonElement generate() throws Exception {
