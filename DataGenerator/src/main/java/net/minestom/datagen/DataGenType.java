c0d442cfa82bfcce8f1491cf581faf96cd07479e
Phillipp Glanz <6745190+TheMeinerLP@users.noreply.github.com>
2024-09-30 13:16:52 +0200

    Rename enchantments to enchantment

--- DataGenerator/src/main/java/net/minestom/datagen/DataGenType.java
+++ DataGenerator/src/main/java/net/minestom/datagen/DataGenType.java
@@ -42,7 +42,7 @@ public enum DataGenType {
     WOLF_VARIANTS("wolf_variants", new GenericResourceGenerator("wolf_variant")),
     TRIM_MATERIALS("trim_materials", new GenericResourceGenerator("trim_material")),
     TRIM_PATTERNS("trim_patterns", new GenericResourceGenerator("trim_pattern")),
-    ENCHANTMENTS("enchantments", new GenericResourceGenerator("enchantment", List.of(), true)),
+    ENCHANTMENTS("enchantment", new GenericResourceGenerator("enchantment", List.of(), true)),
     PAINTING_VARIANTS("painting_variants", new GenericResourceGenerator("painting_variant")),
     JUKEBOX_SONGS("jukebox_songs", new GenericResourceGenerator("jukebox_song")),
 
